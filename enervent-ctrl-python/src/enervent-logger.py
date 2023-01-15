0b167c31d56e80aa44c74ba06f28b97b523354d7
Jarno Rankinen <jarno@oranki.net>
2023-01-15 15:44:26 +0200

    Global debug flag for enervent-logger.py

--- enervent-ctrl-python/src/enervent-logger.py
+++ enervent-ctrl-python/src/enervent-logger.py
@@ -3,6 +3,7 @@ import logging
 from EnerventCoils import PingvinKL
 
 VERSION = "0.0.1"
+DEBUG = False
 
 ## Logging configuration
 log = logging.getLogger(__name__)
@@ -14,7 +15,7 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     log.info(f"Starting enervent-logger {VERSION}")
-    pingvin = PingvinKL('/dev/ttyS0',1,debug=True)
-    print(pingvin.coils.value(1, debug=True))
-    print(pingvin.coils.fetchValue(1, debug=True))
+    pingvin = PingvinKL('/dev/ttyS0',1,debug=DEBUG)
+    print(pingvin.coils.value(1, debug=DEBUG))
+    print(pingvin.coils.fetchValue(1, debug=DEBUG))
     print(pingvin.coils.print())
