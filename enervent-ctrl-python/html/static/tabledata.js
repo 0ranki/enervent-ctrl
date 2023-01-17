3fd6669687fc3c0672be272a5015fedde4356722
Jarno Rankinen <jarno@oranki.net>
2023-01-17 21:05:31 +0200

    Renamed variables, clarified structure

--- enervent-ctrl-python/html/static/tabledata.js
+++ enervent-ctrl-python/html/static/tabledata.js
@@ -13,30 +13,42 @@ function getData() {
     S = zeroPad(now.getSeconds())
     document.getElementById('time').innerHTML = `${Y}-${m}-${d} ${H}:${M}:${S}`
     
+    error = false
+    // The same index.html is used for both coil and register data,
+    // change api url based on which we're looking at
     if (document.location.pathname == "/coils/") {
         url = "/api/v1/coils"
     }
     else if (document.location.pathname == "/registers/") {
         url = "/api/v1/registers"
     }
-    error = false
-    fetch(url)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`Error fetching data: ${response.status}`)
-        }
-        return response.json()
-    })
-    .then((coils) => {
-        document.getElementById('coildata').innerHTML = "";
-        for (n=0; n<coils.length; n++) {
-            tablerow = `<tr><td class="addr" id="addr_${coils[n].address}">${coils[n].address}</td>\
-                            <td class ="val" id="value_${coils[n].address}">${Number(coils[n].value)}</td>\
-                            <td class="symbol" id="symbol_${coils[n].address}">${coils[n].symbol}</td>\
-                            <td class="desc" id="description_${coils[n].address}">${coils[n].description}</td></tr>`
-            document.getElementById('coildata').innerHTML += tablerow
-        }
-    });
+    else {
+        document.getElementById("data").innerHTML = 'Page not found'
+        error = true
+    }
+    if (!error) {
+        // Fetch data from API
+        fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Error fetching data: ${response.status}`)
+            }
+            return response.json()
+        })
+        .then((data) => {
+            // Populate table
+            document.getElementById('coildata').innerHTML = "";
+            for (n=0; n<data.length; n++) {
+                tablerow = `<tr><td class="addr" id="addr_${data[n].address}">${data[n].address}</td>\
+                                <td class ="val" id="value_${data[n].address}">${Number(data[n].value)}</td>\
+                                <td class="symbol" id="symbol_${data[n].address}">${data[n].symbol}</td>\
+                                <td class="desc" id="description_${data[n].address}">${data[n].description}</td></tr>`
+                document.getElementById('coildata').innerHTML += tablerow
+            }
+        });
+    }
 
-    if (!error) setTimeout(getData, 5*1000);
+    // Using setTimeout instead of setInterval to avoid possible connection issues
+    // There's no need to update exactly every 5 seconds, the skew is fine
+    setTimeout(getData, 5*1000);
 }
